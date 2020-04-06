592fea7d29bdbca9fac83737f8e57bea6cae49f6
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-04-06 00:59:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-puts  holiday_hash
+holiday_hash.each do |name, attribute|
+  name.capitalize!
+  attribute.capitalize!
 end
 
 def all_holidays_with_bbq(holiday_hash)
