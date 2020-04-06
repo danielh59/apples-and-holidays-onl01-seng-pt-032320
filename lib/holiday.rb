e8d19eb327d4abb6c21df566e8efc8268d2bcbd1
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-04-06 01:08:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,8 +70,8 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday, supply|
       holiday if supply.include?("BBQ")
-  end
-  .flatten.compact
+   .flatten.compactend
+ 
 end
 end
 
